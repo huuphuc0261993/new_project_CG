a8ed0d98ffaeaffb2d39cfffa5df4164238c2b74
tran phuc <huuphuc.0261993@gmail.com>
2019-09-12 17:14:42 +0700

    upload

---
+++ BIẾN, KIỂU DỮ LIỆU VÀ TOÁN TỬ/Thực hành/[Thực hành] Khai báo và sử dụng biến/bai3.js
@@ -0,0 +1,12 @@
+var a;
+var b;
+
+a = prompt("Nhap a",'nhập số vào');
+b = prompt('nhap b', 'nhập số vào');
+
+if(a % b == 0){
+    alert("a chia hết cho b");
+    }
+else {
+    alert('a không chia hết cho b');
+}
